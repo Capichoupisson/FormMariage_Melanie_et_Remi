e0e537d446fe5f5a9b25d7c4d5e1f9413386691d
Capichoupisson <remi.h.beall@gmail.com>
2025-03-03 14:35:48 +0100

    Update label.jsx

--- components/ui/label.jsx
+++ components/ui/label.jsx
@@ -1,12 +1,11 @@
-const Label = ({ htmlFor, className, children, ...props }) =&gt; {
-return (
-&lt;label
-htmlFor={htmlFor}
-className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed
-peer-disabled:opacity-70 ${className || &#39;&#39;}`}
-{...props}
-&gt;
-{children}
-&lt;/label&gt;
-);
+const Label = ({ htmlFor, className, children, ...props }) => {
+    return (
+        <label
+            htmlFor={htmlFor}
+            className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className || ''}`}
+            {...props}
+        >
+            {children}
+        </label>
+    );
 };
