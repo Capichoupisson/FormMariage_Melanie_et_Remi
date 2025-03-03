981b17db3402bec55e733c24c5fd6d192fb34321
Capichoupisson <remi.h.beall@gmail.com>
2025-03-03 14:33:28 +0100

    Update card.jsx

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -1,10 +1,10 @@
-const Card = ({ className, children, ...props } =&gt); {
-return (
-&lt;div
-className={`rounded-lg border border-gray-200 ${className || &#39;&#39;}`}
-{...props}
-&gt;
-{children}
-&lt;/div&gt;
-);
+const Card = ({ className, children, ...props }) => {
+    return (
+        <div 
+            className={`rounded-lg border border-gray-200 ${className || ''}`}
+            {...props}
+        >
+            {children}
+        </div>
+    );
 };
