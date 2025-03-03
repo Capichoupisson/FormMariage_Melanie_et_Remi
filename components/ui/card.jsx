6a4d5dadc1c44926dddae7cb629492784a88ddaa
Capichoupisson <remi.h.beall@gmail.com>
2025-03-03 11:05:20 +0100

    Create card.jsx

---
+++ components/ui/card.jsx
@@ -0,0 +1,10 @@
+const Card = ({ className, children, ...props }) =&gt; {
+return (
+&lt;div
+className={`rounded-lg border border-gray-200 ${className || &#39;&#39;}`}
+{...props}
+&gt;
+{children}
+&lt;/div&gt;
+);
+};
